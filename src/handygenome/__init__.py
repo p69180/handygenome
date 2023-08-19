c7e4d9ec36ae06b10eb17367e0e7fc2928a849f5
Ji-Hyung Park <p69180@gmail.com>
2023-08-20 01:24:07 +0900

    Create setup.py

--- src/handygenome/__init__.py
+++ src/handygenome/__init__.py
@@ -1,6 +1,8 @@
 import sys
 import os
+import pathlib
 import importlib
+import importlib.resources
 import importlib.metadata
 
 import yaml
@@ -19,23 +21,37 @@ os.environ["VECLIB_MAXIMUM_THREADS"] = "1"  # export VECLIB_MAXIMUM_THREADS=4
 os.environ["NUMEXPR_NUM_THREADS"] = "1"  # export NUMEXPR_NUM_THREADS=6
 
 
-# constants
-PROJECT_PATH = os.path.dirname(os.path.dirname(handygenome.__file__))  
-    # /home/users/pjh/scripts/python_genome_package_dev
-DIRS = {
-    'data': os.path.join(PROJECT_PATH, 'data'),
-    'externals': os.path.join(PROJECT_PATH, 'externals'),
-    'R': os.path.join(PROJECT_PATH, 'R'),
-    'scripts': os.path.join(PROJECT_PATH, 'scripts'),
-    'package': os.path.join(PROJECT_PATH, 'handygenome'),
-}
+# USERDIR (~/.handygenome)
+USERDIR = pathlib.Path.home() / '.handygenome'
 
-
-# load configuration
-CONFIG_PATH = os.path.join(PROJECT_PATH, 'config.yaml')
-with open(CONFIG_PATH) as f:
+CONFIGPATH = USERDIR / 'config.yaml'
+with CONFIGPATH.open() as f:
     OPTION = yaml.safe_load(f)
 if OPTION['python'] is None:
     OPTION['python'] = sys.executable
-    
-    
+
+DATADIR = USERDIR / 'data'
+
+
+#PROJECT_PATH = os.path.dirname(os.path.dirname(handygenome.__file__))  
+#    # /home/users/pjh/scripts/python_genome_package_dev
+
+# data files within top package
+PKGPATH = importlib.resources.files(handygenome)
+DIRS = {
+    #'data': os.path.join(PROJECT_PATH, 'data'),
+    #'externals': os.path.join(PROJECT_PATH, 'externals'),
+    'R': PKGPATH.joinpath('Rscripts'),
+    'scripts': PKGPATH.joinpath('scripts'),
+    #'package': os.path.join(PROJECT_PATH, 'handygenome'),
+}
+
+#DIRS = {
+#    'data': os.path.join(PROJECT_PATH, 'data'),
+#    'externals': os.path.join(PROJECT_PATH, 'externals'),
+#    'R': os.path.join(PROJECT_PATH, 'R'),
+#    'scripts': os.path.join(PROJECT_PATH, 'scripts'),
+#    'package': os.path.join(PROJECT_PATH, 'handygenome'),
+#}
+
+
